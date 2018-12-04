55cafb750b6e2b84a9f75828fef083b3eda9223d
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-12-04 13:09:05 -0600

    add post compile script, adjust settings

--- djoauth/djoauth/settings.py
+++ djoauth/djoauth/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dls8o_6c=-!bhnt)(nxhpj7xy=ntkfxgj-vvsh+dr$#tt5@bt@'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ast.literal_eval(os.environ['DEBUG_STATE'])
 
 ALLOWED_HOSTS = []
 
@@ -75,12 +75,7 @@ WSGI_APPLICATION = 'djoauth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
